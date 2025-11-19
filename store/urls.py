240ef420bf353f3c8439fe392c4d8a3d67012d80
SergeiCH <cheremushnikoff@yandex.ru>
2025-11-20 01:37:04 +0800

    Создал представление и шаблон отображения товаров и категорий

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import homepage_view
+from .views import homepage_view, product_list
 
 urlpatterns = [
-    path('', homepage_view, name='home')
+    path('catalog/', product_list, name='catalog'),
+    path('catalog/<slug:category_slug>', product_list, name='catalog_category'),
+    path('', homepage_view, name='home'),
+
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 def homepage_view(request):
     return render(request, 'store/home_page.html')
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    catalog = Product.objects.all()
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        catalog = Product.objects.filter(category=category)
+
+    context = {
+        'category': category,
+        'categories': categories,
+        'catalog': catalog
+    }
+
+    return render(request, 'store/catalog.html', context)
